0f0a311e1f38fe160b877d579c4710a533e1ce9a
lmaddalena <mdn.luca@gmail.com>
2019-02-27 13:46:27 +0100

    renamed ex1.cs in program.cs

---
+++ ex1_multi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ex1_multi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
